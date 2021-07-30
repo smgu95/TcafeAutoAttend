5446316e0a11bfe60e4420646f51a682e1a876a0
정기환 <smgu95@gmail.com>
2021-07-30 23:25:32 +0900

    Update autoClick.py

--- autoClick.py
+++ autoClick.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common import keys
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver import ActionChains
 import time
 
 driver = webdriver.Chrome('chromedriver.exe')
