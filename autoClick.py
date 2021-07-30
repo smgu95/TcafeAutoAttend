227197b52fbfc07b12e2f1678f14ea898f797ade
정기환 <smgu95@gmail.com>
2021-07-30 23:25:09 +0900

    Add files via upload

---
+++ autoClick.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.common import keys
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import ActionChains
+import time
+
+driver = webdriver.Chrome('chromedriver.exe')
+url = 'http://tcafe2a.com/'
+driver.get(url)
+
+
+id=driver.find_element_by_id('ol_id')
+id.send_keys('아이디 입력')
+pw=driver.find_element_by_id('ol_pw')
+pw.send_keys('패스워드 입력')
+
+loginbutton = driver.find_element_by_tag_name('button')
+loginbutton.click()
+
+
+driver.get(url+'/community/attendance')
+
+driver.find_element_by_xpath('//*[@id="cnftjr"]/div/form/table/tbody/tr/td/img').click()
+
